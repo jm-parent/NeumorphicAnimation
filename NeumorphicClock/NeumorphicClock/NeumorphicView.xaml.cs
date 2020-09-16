c226f53f30291671e76fdeaa29db99877d82987d
Jean-Marie PARENT <jmparent.github@gmail.com>
2020-09-16 17:15:23 +0200

    update

--- NeumorphicClock/NeumorphicClock/NeumorphicView.xaml.cs
+++ NeumorphicClock/NeumorphicClock/NeumorphicView.xaml.cs
@@ -7,61 +7,58 @@ using System.Threading.Tasks;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
 using Xamarin.Forms.Xaml;
 
 namespace NeumorphicClock
 {
-
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NeumorphicView : ShadowsElement
     {
+        private PancakeView firstAnimationHolder = new PancakeView();
+        private PancakeView secondAnimationHolder = new PancakeView();
         public NeumorphicView()
         {
             InitializeComponent();
             BindingContext = new NeuViewModel();
 
             Debug.WriteLine($"Neumorphism view BindingContext: {BindingContext}");
-         
-            PancakeView.BackgroundGradientStartPoint = new Point(0,0);
+
+            PancakeView.BackgroundGradientStartPoint = new Point(0, 0);
             PancakeView.BackgroundGradientEndPoint = new Point(1, 1);
             GradientOne.Color = Color.FromHex("ffffff");
             GradientTwo.Color = Color.FromHex("d8d8db");
         }
 
         public bool clicked { get; set; }
-        private bool _isAnimate;
+
         private async void Animation()
         {
-            //e7f3ea
-            //c2ccc5
             var colorOne = Color.FromHex("ffffff");
             var colorTwo = Color.FromHex("d8d8db");
-            var fade = 1.0;
-            if(!clicked)
-            await Task.WhenAll(
-                this.ColorTo(colorOne, colorTwo, c => GradientOne.Color = c, 200),
-                PancakeView.ColorTo(colorTwo, colorOne, c => GradientTwo.Color = c, 200));
+          
+            if (!clicked)
+                await Task.WhenAll(
+                    firstAnimationHolder.ColorTo(colorOne, colorTwo, c => GradientOne.Color = c, 200),
+                    secondAnimationHolder.ColorTo(colorTwo, colorOne, c => GradientTwo.Color = c, 200));
             else
             {
                 await Task.WhenAll(
-                    this.ColorTo(colorTwo, colorOne, c => GradientOne.Color = c, 200),
-                    PancakeView.ColorTo(colorOne,colorTwo , c => GradientTwo.Color = c, 200));
+                    firstAnimationHolder.ColorTo(colorTwo, colorOne, c => GradientOne.Color = c, 200),
+                    secondAnimationHolder.ColorTo(colorOne, colorTwo, c => GradientTwo.Color = c, 200));
             }
-
 
             clicked = !clicked;
         }
+
         void OnCancelAnimationButtonClicked(object sender, EventArgs e)
         {
         }
-        public override void OnIsCompactChanged()
-        {
-            if (IsCompact)
-            {
-                Description.Height = 0;
-            }
-        }
+
+   
+
         private Animation _animation;
+
         private void ImageButtonOnClicked(object sender, EventArgs e)
         {
             Debug.WriteLine($"ButtonPlusNeuShadows BindingContext: {ButtonPlusNeuShadows.BindingContext}");
@@ -71,11 +68,8 @@ namespace NeumorphicClock
             }
 
             Animation();
-
         }
-
     }
-
 
 
     public class NeuViewModel
